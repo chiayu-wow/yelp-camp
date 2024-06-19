9f294470e2f5ec3dca77aadacce4af57ebc12e8c
Yoyo <yoyowamg1012@gmail.com>
2024-06-19 21:20:00 +0800

    fix errors

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet')
 const MongoStore = require('connect-mongo');
 const dbUrl = process.env.DB_URL || 'mongodb://127.0.0.1:27017/yelp-camp'
-const secret = process.env.secret || 'thisismysecret'
+const secret = process.env.SECRET || 'thisismysecret'
 
 const store = MongoStore.create({
     mongoUrl: dbUrl,
